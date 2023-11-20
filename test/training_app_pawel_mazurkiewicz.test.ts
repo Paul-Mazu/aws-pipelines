6a255d23ea093dd89d848d9dc1a8f44b1a65b10c
EC2 Default User <ec2-user@ip-172-31-40-196.eu-west-1.compute.internal>
2023-11-20 14:51:43 +0000

    Initial commit

---
+++ test/training_app_pawel_mazurkiewicz.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrainingAppPawelMazurkiewicz from '../lib/training_app_pawel_mazurkiewicz-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/training_app_pawel_mazurkiewicz-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrainingAppPawelMazurkiewicz.TrainingAppPawelMazurkiewiczStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
